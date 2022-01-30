2a8a88e182ae3c7fd65a86a4ccc45247118340e6
Michael <michael@ou.edu>
2022-01-30 12:01:37 -0600

    Finished problem 4

--- Homework 1 Code/H_1_4.py
+++ Homework 1 Code/H_1_4.py
@@ -2,8 +2,19 @@
 import numpy as np
 
 def make_matrix(m, n):
-    temp = np.matrix[m, n]
+    '''
+
+    :param m: The number of rows in the matrix
+    :param n: The number of columns in the matrix
+    :return: A matrix with each position filled with 2i + j
+    '''
+
+    temp = np.empty((m, n))
+    #Loop through the array
+    for i in range(m):
+        for j in range (n):
+            temp[i, j] = 2*(i+1) + (j+1)
     return temp
 
 if __name__ == '__main__':
-    print(make_matrix(3, 3))
+    print(make_matrix(3, 4))
